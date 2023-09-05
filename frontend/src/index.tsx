7f2ab688851728cde3864a642c10d3db23f209cb
Benedikt Hofirek <benedikthofirek@gmail.com>
2023-09-05 13:15:09 +0200

    project structure setup

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,23 +1,5 @@
 import { render } from 'preact';
-import { LocationProvider, Router, Route } from 'preact-iso';
-
-import { Header } from './components/Header.js';
-import { Home } from './pages/Home/index.js';
-import { NotFound } from './pages/_404.js';
-import './style.css';
-
-export function App() {
-	return (
-		<LocationProvider>
-			<Header />
-			<main>
-				<Router>
-					<Route path="/" component={Home} />
-					<Route default component={NotFound} />
-				</Router>
-			</main>
-		</LocationProvider>
-	);
-}
+import './styles.scss';
+import App from "@/App"
 
 render(<App />, document.getElementById('app'));
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
 	plugins: [preact()],
+	resolve: {
+		alias: {
+			"@": "/src",
+		},
+	},
 });
